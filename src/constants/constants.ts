c597ca87bbaa1341637ef45ff777822eac99a8ce
anton13602 <an-kochetov@mail.ru>
2022-08-23 21:11:26 +0300

    feat: add constant url and maximum number of characters for a password

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -3,3 +3,7 @@ import { IConstants } from '../types/types';
 export const constants: IConstants = {
   DEFAULT_PAGE: 'main',
 };
+
+export const BASE_URL: string = 'https://app-rslang-exsample.herokuapp.com';
+
+export const MAX_COUNT_OF_SYMBOLS: number = 8;
