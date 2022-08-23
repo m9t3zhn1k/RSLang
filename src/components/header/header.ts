914816944773b5f358b2aaf7ee231fb761616f03
anton13602 <an-kochetov@mail.ru>
2022-08-23 21:41:12 +0300

    feat: add button for login User

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -1,13 +1,35 @@
 import { BaseComponent } from '../../components/base-component/base-component';
 import { IBaseComponent } from '../../types/types';
+import Authorization from '../authorization/authorization';
 import { Navigation } from '../navigation/navigation';
 import './header.scss';
 
 export class Header extends BaseComponent implements IBaseComponent {
   public readonly navigation: Navigation;
 
+  public buttonSingIn: BaseComponent;
+
+  private authorization: Authorization | undefined;
+
   constructor(parent: HTMLElement) {
     super(parent, 'header', ['header']);
     this.navigation = new Navigation(this.element);
+
+    this.buttonSingIn = new BaseComponent(this.element, 'button', ['header__login'], 'Войти');
+    this.buttonSingIn.element.addEventListener('click', (): void => {
+      switch (this.buttonSingIn.element.textContent) {
+        case 'Войти':
+          this.authorization = new Authorization(document.body, this.changeContentForButtonLogin.bind(this));
+          break;
+        case 'Выйти':
+          this.authorization?.signOut();
+          this.changeContentForButtonLogin('Войти');
+          break;
+      }
+    });
+  }
+
+  changeContentForButtonLogin(content: string): void {
+    this.buttonSingIn.element.textContent = content;
   }
 }
