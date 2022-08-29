24a6dd4f43753eb2f97719edf2bafc299a1f4122
anton13602 <an-kochetov@mail.ru>
2022-08-29 13:12:39 +0300

    feat: add constant for level language

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,4 +1,4 @@
-import { IConstants } from '../types/types';
+import { IConstants, ILanguageLevels } from '../types/types';
 
 export const constants: IConstants = {
   DEFAULT_PAGE: 'main',
@@ -7,3 +7,12 @@ export const constants: IConstants = {
 export const BASE_URL: string = 'https://app-rslang-exsample.herokuapp.com';
 
 export const MAX_COUNT_OF_SYMBOLS: number = 8;
+
+export const LANGUAGE_LEVELS: ILanguageLevels = {
+  A1: 0,
+  A2: 1,
+  B1: 2,
+  B2: 3,
+  C1: 4,
+  C2: 5,
+};
