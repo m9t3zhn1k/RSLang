5c2ed8935e7c813688a556ab000daa0722278d52
anton13602 <an-kochetov@mail.ru>
2022-08-31 21:49:43 +0300

    feat: add methods for get words

--- src/components/audiochallenge-page/audiochallenge-page.ts
+++ src/components/audiochallenge-page/audiochallenge-page.ts
@@ -1,4 +1,4 @@
-import { BASE_URL, LANGUAGE_LEVELS } from '../../constants/constants';
+import { LANGUAGE_LEVELS } from '../../constants/constants';
 import { getWords } from '../../controller/words-controller';
 import { IWord } from '../../types/types';
 import { BaseComponent } from '../base-component/base-component';
@@ -6,23 +6,44 @@ import imgSound from '../../assets/sound.svg';
 import './audiochallenge-page.scss';
 import { Router } from '../../router/router';
 import AudioChallengeGame from './audio-challenge-game/audio-challenge-game';
+import { getUserAgrGameWords, getUserId } from '../../controller/user-controller';
 
 export class AudioChallengePage extends BaseComponent {
   private words: IWord[] = [];
 
   private levelGame: null | number = null;
 
-  constructor(protected parentNode: HTMLElement, private router: Router, public data?: IWord[], public level?: number) {
+  constructor(protected parentNode: HTMLElement, private router: Router, public data?: IWord[]) {
     super(parentNode, 'div', ['audio-challenge']);
 
-    if (data && level) {
+    if (data) {
       this.words = data;
-      this.levelGame = level;
       this.renderGame();
     } else {
-      this.renderLights();
       this.renderStartGame();
     }
+    this.renderLights();
+  }
+
+  private async getGameWords(group: number, page: number): Promise<IWord[]> {
+    let words: IWord[] = [];
+    if (getUserId()) {
+      words = (await getUserAgrGameWords(group)).filter((word: IWord): boolean => {
+        return !word.userWord?.optional?.isLearned && word.page <= page;
+      });
+    } else {
+      while (page >= 0) {
+        words = words.concat(await getWords(group, page));
+        page -= 1;
+      }
+    }
+    words.sort((a: IWord, b: IWord): number => b.page - a.page);
+    return words;
+  }
+
+  private async initGame(group: number, page: number): Promise<void> {
+    this.words = await this.getGameWords(group, page);
+    this.words = this.words.slice(0, 20);
   }
 
   private generateRandomNum(): number {
@@ -86,9 +107,9 @@ export class AudioChallengePage extends BaseComponent {
     });
 
     startGameButton.element.addEventListener('click', async (): Promise<void> => {
-      const page = this.generateRandomNum();
+      const page: number = this.generateRandomNum();
       if (this.levelGame !== null) {
-        const data: IWord[] = await getWords(page, this.levelGame);
+        const data: IWord[] = await getWords(this.levelGame, page);
         this.words = [];
         this.words.push(...data);
         wrapperStart.remove();
@@ -103,16 +124,7 @@ export class AudioChallengePage extends BaseComponent {
 
   private renderResultGame(arrRightWord: number[], arrWrongWord: number[]): void {
     const wrapperResult: BaseComponent = new BaseComponent(this.element, 'div', ['audio-challenge__result']);
-    const rightWord: BaseComponent = new BaseComponent(wrapperResult.element, 'div', ['result__right']);
-    const wrongWord: BaseComponent = new BaseComponent(wrapperResult.element, 'div', ['result__wrong']);
-
-    new BaseComponent(rightWord.element, 'h4', ['result__title'], `ЗНАЮ: ${arrRightWord.length}`);
-    this.renderResultItem(arrRightWord, rightWord.element);
-
-    new BaseComponent(wrongWord.element, 'h4', ['result__title'], `ОШИБОК: ${arrWrongWord.length}`);
-    this.renderResultItem(arrWrongWord, wrongWord.element);
-
-    const wrapperResultButtons: BaseComponent = new BaseComponent(this.element, 'div', ['result__buttons']);
+    const wrapperResultButtons: BaseComponent = new BaseComponent(wrapperResult.element, 'div', ['result__buttons']);
     const buttonRepeat: BaseComponent = new BaseComponent(
       wrapperResultButtons.element,
       'button',
@@ -125,6 +137,14 @@ export class AudioChallengePage extends BaseComponent {
       ['audio-challenge__btn', 'result__but'],
       'К списку игр'
     );
+    const rightWord: BaseComponent = new BaseComponent(wrapperResult.element, 'div', ['result__right']);
+    const wrongWord: BaseComponent = new BaseComponent(wrapperResult.element, 'div', ['result__wrong']);
+
+    new BaseComponent(rightWord.element, 'h4', ['result__title'], `ЗНАЮ: ${arrRightWord.length}`);
+    this.renderResultItem(arrRightWord, rightWord.element);
+
+    new BaseComponent(wrongWord.element, 'h4', ['result__title'], `ОШИБОК: ${arrWrongWord.length}`);
+    this.renderResultItem(arrWrongWord, wrongWord.element);
 
     buttonRepeat.element.addEventListener('click', (): void => {
       wrapperResult.remove();
@@ -142,7 +162,7 @@ export class AudioChallengePage extends BaseComponent {
     wordsNum.forEach((word: number): void => {
       const itemWrapper: BaseComponent = new BaseComponent(parentElement, 'div', ['word-item']);
       const audio: HTMLAudioElement = document.createElement('audio');
-      audio.src = `${BASE_URL}/${this.words[word].audio}`;
+      audio.src = this.words[word].audio;
 
       const soundItem: BaseComponent = new BaseComponent(itemWrapper.element, 'div', ['word-item__sound']);
       const buttonAudioCard: BaseComponent = new BaseComponent(soundItem.element, 'button', [
@@ -157,8 +177,14 @@ export class AudioChallengePage extends BaseComponent {
       new BaseComponent(
         itemWrapper.element,
         'span',
+        ['word-item__word-transcript'],
+        `${this.words[word].transcription} `
+      );
+      new BaseComponent(
+        itemWrapper.element,
+        'span',
         ['word-item__word-translate'],
-        ` — ${this.words[word].wordTranslate}`
+        `${this.words[word].wordTranslate}`
       );
     });
   }
