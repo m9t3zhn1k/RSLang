94cf76b471b56aaefa5dc94b03c39220de5068c5
anton13602 <an-kochetov@mail.ru>
2022-09-02 10:48:44 +0300

    feat: add loader component

---
+++ src/components/loader/loader.ts
@@ -0,0 +1,19 @@
+import { BaseComponent } from '../base-component/base-component';
+import './loader.scss';
+
+class Loader {
+  public isFetching: boolean = false;
+
+  private preloaderWrapper?: BaseComponent;
+
+  public createLoader(parentElement: HTMLElement): void {
+    this.preloaderWrapper = new BaseComponent(parentElement, 'div', ['preloader']);
+    new BaseComponent(this.preloaderWrapper.element, 'div', ['preloader-loading']);
+  }
+
+  public destroy(): void {
+    this.preloaderWrapper?.remove();
+  }
+}
+
+export default Loader;
